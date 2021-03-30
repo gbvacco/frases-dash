c44d96addce9fd8465e899f0610f15cdbb97e9ad
Giovanni <gbvacco@hotmail.com>
2021-03-29 23:43:41 -0300

    primeiro painel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { FormControl, FormGroup } from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'frases-dash';
+  constructor(private firestore: AngularFirestore) { }
+  public title = 'frases-dash';
+
+  public form = new FormGroup({
+    titulo: new FormControl(''),
+    frase: new FormControl(''),
+    autor: new FormControl(''),
+    autor_url: new FormControl(''),
+    usuario_rede_social: new FormControl(''),
+    link_usuario_rede_social: new FormControl(''),
+    nome_livro: new FormControl(''),
+    autor_livro: new FormControl(''),
+    resumo_livro: new FormControl(''),
+    livro_url: new FormControl(''),
+    likes: new FormControl(''),
+    data_exibicao: new FormControl('')
+  });
+
+  private convertDateToDoc(chooseDate: string) {
+    const splitDate = chooseDate.split('-');
+    return `${splitDate[2]}${splitDate[1]}${splitDate[0]}`
+  }
+
+  public add() {
+    this.firestore
+      .collection('frases')
+      .doc(this.convertDateToDoc(this.form.get('data_exibicao')?.value))
+      .set(this.form.value)
+      .then(res => {
+        console.log(res);
+        // this.form.reset();
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCsTfDyAV8z72u0UlB8dHVOKKvrguReMTY",
+  authDomain: "frases-2e227.firebaseapp.com",
+  projectId: "frases-2e227",
+  storageBucket: "frases-2e227.appspot.com",
+  messagingSenderId: "793182217084",
+  appId: "1:793182217084:web:bc055799152a99f6a4faf9",
+  measurementId: "G-825H7VPRQH"
+};
 
 @NgModule({
   declarations: [
@@ -10,7 +23,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule,
+    AngularFirestoreModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
